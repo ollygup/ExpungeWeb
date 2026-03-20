ce5c567b1829fea5c5e93ef2d97f2cab748a9f15
Ollygup <82217636+ollygup@users.noreply.github.com>
2026-03-20 17:11:45 +0800

    fix

--- src/utils/custom-logger.ts
+++ src/utils/custom-logger.ts
@@ -1,9 +1,11 @@
-import { environment } from "../environments/environment";
+// workers ignore environment.ts/environment.prod.ts replacement done by angular
+// so use this instead
+const isProd = import.meta.env['NG_APP_ENV'] === 'production';
 
 export const customLogger = {
-    log: environment.production ? () => { } : console.log.bind(console),
-    debug: environment.production ? () => { } : console.debug.bind(console),
-    warn: environment.production ? () => { } : console.warn.bind(console),
-    info: environment.production ? () => { } : console.info.bind(console),
-    error: console.error.bind(console), // always on
+  log:   isProd ? () => {} : console.log.bind(console),
+  debug: isProd ? () => {} : console.debug.bind(console),
+  warn:  isProd ? () => {} : console.warn.bind(console),
+  info:  isProd ? () => {} : console.info.bind(console),
+  error: console.error.bind(console),
 };
